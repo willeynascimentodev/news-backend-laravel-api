9439924f4f7d889ed8dec8d5ba83b3adab9b7453
Willey Nascimento <willeynascimentodev@waan.com.br>
2023-06-13 06:02:26 -0300

    Login endpoint with JWT Token

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class AuthController extends Controller
+{
+    public function store(Request $req) {
+        $credentials = request(['email', 'password']);
+
+        if (! $token = auth()->attempt($credentials)) {
+            return response()->json(['error' => 'Invalid Credentials'], 401);
+        }
+
+        return response()->json([
+            'data' => [
+                'token' => $token,
+                'email' => $req->email
+            ],
+            'message' => 'Token generated'
+            ], 201);
+    }
+
+    public function destroy(Request $req) {
+        auth()->logout();
+        return response()->json(['message' => 'Successfully logged out']);
+    }
+}
--- setup.sh
+++ setup.sh
@@ -2,4 +2,8 @@ composer install
 docker compose up -d
 docker compose exec -i news-laravel-api cp .env.example .env
 docker compose exec -i news-laravel-api php artisan key:generate
+docker compose exec -i news-laravel-api php artisan jwt:secret
 docker compose exec -i news-laravel-api php artisan config:clear
+
+docker compose exec -i backend-news-laravel-api php artisan migrate:fresh --env=local
+docker compose exec -i backend-news-laravel-api php artisan db:seed --env=local
